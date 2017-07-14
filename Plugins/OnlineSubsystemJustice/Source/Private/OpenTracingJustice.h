d15273b0f275085e5e01bc051b08c7213be498a9
Cove Schneider <covecs@gmail.com>
2017-07-13 22:17:08 -0700

    formatting

--- Plugins/OnlineSubsystemJustice/Source/Private/OpenTracingJustice.h
+++ Plugins/OnlineSubsystemJustice/Source/Private/OpenTracingJustice.h
@@ -11,8 +11,10 @@ class FOpenTracingJustice
 public:
 
 	FOpenTracingJustice()
-	{
-	}
+	{ }
+
+	~FOpenTracingJustice()
+	{}
 
 	FOpenTracingJustice(const FOpenTracingJustice& Parent)
 	{
@@ -29,14 +31,12 @@ public:
 		FGuid NewSpan = FGuid::NewGuid();
 		SpanId = FGuid(0, 0, NewSpan[2], NewSpan[3]);
 	}
-
 	
 	bool IsValid()   const { return TraceId.IsValid(); };
 
 	FGuid GetTraceId()  const { return TraceId;  };
 	FGuid GetSpanId()   const { return SpanId;   };
 	FGuid GetParentSpanId() const { return ParentSpanId; };
-
 	FString GetTraceIdStr()  const { return FString::Printf(TEXT("%08x%08x%08x%08x"), TraceId[0], TraceId[1], TraceId[2], TraceId[3]); };
 	FString GetSpanIdStr()   const { return FString::Printf(TEXT("%08x%08x"), SpanId[2], SpanId[3]); };
 	FString GetParentSpanIdStr() const { return FString::Printf(TEXT("%08x%08x"), ParentSpanId[2], ParentSpanId[3]); };
@@ -46,10 +46,6 @@ public:
 		return FString::Printf(TEXT("TraceId=%s SpanId=%s ParentSpanId=%s"),
 							   *GetTraceIdStr(), *GetSpanIdStr(), *GetParentSpanIdStr());
 	};
-
-	~FOpenTracingJustice()
-	{
-	}
 
 private:
 	
