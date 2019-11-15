a4e2cc0a03c0c126a5922b7913a201b6a68823c9
adityakurniap <adityakurniap@bitbucket.org>
2019-11-15 16:07:45 +0700

    docs(doxygen): delete doxygen and all file that relate to that

--- Jenkins/PublishDocumentation.bat
+++
@@ -1,9 +0,0 @@
-::Generate and upload inlined documentation
-
-::::Generate doxygen documentation
-pushd Documentation
-doxygen Doxyfile
-popd
-
-::::Upload the generated documentation
-aws s3 cp "%WORKSPACE%\Doxygen\html" s3://docs.accelbyte.net/sdk/ue4/ --recursive
