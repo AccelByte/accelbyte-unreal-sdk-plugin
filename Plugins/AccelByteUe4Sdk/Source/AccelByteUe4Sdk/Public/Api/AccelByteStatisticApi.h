8581ca50f8834906fc9092cdf8d1d02c6a1580ee
TeoFerdian <teoderikus@accelbyte.net>
2020-01-27 15:57:01 +0700

    feat(statistic): simplify statistic and server statistic

--- Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Public/Api/AccelByteStatisticApi.h
+++ Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Public/Api/AccelByteStatisticApi.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 AccelByte Inc. All Rights Reserved.
+// Copyright (c) 2019 - 2020 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
@@ -31,73 +31,48 @@ namespace AccelByte
 			const Credentials& Credentials;
 			const Settings& Settings;
 
-		public:
+public:
+	/**
+	* @brief Create a stat item.
+	*
+	* @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsBulkStatItemOperationResult.
+	* @param OnError This will be called when the operation failed.
+	*/
+	void CreateUserStatItems(const TArray<FString>& StatCodes, const THandler<TArray<FAccelByteModelsBulkStatItemOperationResult>>& OnSuccess, const FErrorHandler& OnError);
 
-			/**
-			 * @brief Get all stat items from a specified user id.
-			 *
-			 * @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsUserStatItemPagingSlicedResult.
-			 * @param OnError This will be called when the operation failed.
-			*/
-			void GetAllUserStatItems(const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
+	/**
+		* @brief Get all this user stat items.
+		*
+		* @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsUserStatItemPagingSlicedResult.
+		* @param OnError This will be called when the operation failed.
+	*/
+	void GetAllUserStatItems(const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
 
-			/**
-			 * @brief Get all stat items from a specified user id and statCode(s).
-			 *
-			 * @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsUserStatItemPagingSlicedResult.
-			 * @param OnError This will be called when the operation failed.
-			*/
-			void GetUserStatItemsByStatCodes(const TArray<FString>& StatCodes, const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
+	/**
+		* @brief Get this user stat items by specifying statCodes and tags to get from. Returned stat items will only contain
+		* stat items specified by statCodes and tags (inclusive)
+		*
+		* @param statCodes Specify statCodes for stat items to get from this method
+		* @param tags Specify tags for for stat items to get from this method
+		* @param OnSuccess This will be called when the operation succeeded. Thre result is an FAccelByteModelsUserStatItemPagingSlicedResult
+		* @param OnError This will be called when the operation failed.
+	*/
+	void GetUserStatItems(const TArray<FString>& StatCodes, const TArray<FString>& Tags, const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
 
-			/**
-			 * @brief Get all stat items from a specified user id and tag(s).
-			 *
-			 * @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsUserStatItemPagingSlicedResult.
-			 * @param OnError This will be called when the operation failed.
-			*/
-			void GetUserStatItemsByTags(const TArray<FString>& Tags, const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
+	/**
+	* @brief Increment this user stat items.
+	*
+	* @param Data array consist of increased value and stat code.
+	* @param OnSuccess This will be called when the operation succeeded. The result is an array of FAccelByteModelsBulkStatItemOperationResult.
+	* @param OnError This will be called when the operation failed.
+	*/
+	void IncrementUserStatItems(const TArray<FAccelByteModelsBulkStatItemInc>& Data, const THandler<TArray<FAccelByteModelsBulkStatItemOperationResult>>& OnSuccess, const FErrorHandler& OnError);
 
-			/**
-			* @brief Bulk add stat item(s) value from specified user id(s) by stat code(s).
-			*
-			* @param Data array consist of increased value, user id, and stat code.
-			* @param OnSuccess This will be called when the operation succeeded. The result is an array of FAccelByteModelsBulkStatItemOperationResult.
-			* @param OnError This will be called when the operation failed.
-			*/
-			void BulkAddStatItemValue(const TArray<FAccelByteModelsBulkUserStatItemInc>& Data, const THandler<TArray<FAccelByteModelsBulkStatItemOperationResult>>& OnSuccess, const FErrorHandler& OnError);
-
-			/**
-			* @brief Bulk add stat item(s) value from a specified user id by stat code(s).
-			*
-			* @param Data array consist of increased value and stat code.
-			* @param OnSuccess This will be called when the operation succeeded. The result is an array of FAccelByteModelsBulkStatItemOperationResult.
-			* @param OnError This will be called when the operation failed.
-			*/
-			void BulkAddUserStatItemValue(const TArray<FAccelByteModelsBulkStatItemInc>& Data, const THandler<TArray<FAccelByteModelsBulkStatItemOperationResult>>& OnSuccess, const FErrorHandler& OnError);
-
-			/**
-			* @brief Add a stat item value from a specified user id by stat code.
-			*
-			* @param StatCode the statCode of statItem that about to update.
-			* @param Inc increased value of the statItem.
-			* @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsBulkStatItemOperationResult.
-			* @param OnError This will be called when the operation failed.
-			*/
-			void AddUserStatItemValue(const FString& StatCode, const float& Inc, const THandler<FAccelByteModelsStatItemIncResult>& OnSuccess, const FErrorHandler& OnError);
-
-		private:
-			Statistic() = delete;
-			Statistic(Statistic const&) = delete;
-			Statistic(Statistic&&) = delete;
-
-			/**
-			 * @brief Get all stat items from a specified user id.
-			 *
-			 * @param OnSuccess This will be called when the operation succeeded. The result is an FAccelByteModelsUserStatItemPagingSlicedResult.
-			 * @param OnError This will be called when the operation failed.
-			*/
-			void GetUserStatItems(const TArray<FString>& StatCodes, const TArray<FString>& Tags, const THandler<FAccelByteModelsUserStatItemPagingSlicedResult>& OnSuccess, const FErrorHandler& OnError);
-		};
+private:
+	Statistic() = delete;
+	Statistic(Statistic const&) = delete;
+	Statistic(Statistic&&) = delete;
+};
 
 	} // Namespace Api
 } // Namespace AccelByte
