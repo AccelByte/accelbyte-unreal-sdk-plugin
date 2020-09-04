221752133149bc0c34487ad5abeb754ad3cce73b
Narendra <narendra@accelbyte.net>
2020-09-04 15:11:09 +0700

    test(ecommerce): wrong UUIDv4 format

--- Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Private/Tests/EcommerceIntegrationTest.cpp
+++ Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Private/Tests/EcommerceIntegrationTest.cpp
@@ -1240,7 +1240,7 @@ bool EcommerceGetUserEntitlementByEntitlementIdInvalid::RunTest(const FString& P
 	bool bGetEntitlementDone = false;
 	bool bGetEntitlementError = false;
 	UE_LOG(LogAccelByteEcommerceTest, Log, TEXT("GetUserEntitlementSuccess"));
-	FRegistry::Entitlement.GetUserEntitlementById("440e4476-19a4-4658-a95f-d6607204c478",
+	FRegistry::Entitlement.GetUserEntitlementById("440e447619a44658a95fd6607204c478",
 		THandler<FAccelByteModelsEntitlementInfo>::CreateLambda([&](const FAccelByteModelsEntitlementInfo& Result)
 	{
 		UE_LOG(LogAccelByteEcommerceTest, Log, TEXT("    Success"));
