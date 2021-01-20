e936a5f75aecd365d12dcb3ec3653823197a3c5a
Leonardus Windiono <windio@accelbyte.net>
2021-01-20 10:34:19 +0700

    fix(AccelByteUe4SdkDemo): fixed build for console

--- Source/AccelByteUe4SdkDemo/AccelByteUe4SdkDemo.Build.cs
+++ Source/AccelByteUe4SdkDemo/AccelByteUe4SdkDemo.Build.cs
@@ -2,6 +2,7 @@
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
+using System;
 using UnrealBuildTool;
 
 public class AccelByteUe4SdkDemo : ModuleRules
@@ -16,8 +17,10 @@ public class AccelByteUe4SdkDemo : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AccelByteUe4Sdk", "Agones", "Json", "JsonUtilities", "Http", "WebSockets",});
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
-        DynamicallyLoadedModuleNames.AddRange(new string[] { "OnlineSubsystemSteam", }
-            );
+
+        // Uncomment if you are using steam
+        // DynamicallyLoadedModuleNames.AddRange(new string[] { "OnlineSubsystemSteam",});
+
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
--- Source/AccelByteUe4SdkDemoEditor.Target.cs
+++ Source/AccelByteUe4SdkDemoEditor.Target.cs
@@ -9,6 +9,9 @@ public class AccelByteUe4SdkDemoEditorTarget : TargetRules
 {
     public AccelByteUe4SdkDemoEditorTarget(TargetInfo Target) : base(Target)
     {
+#if UE_4_24_OR_LATER
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+#endif
         Type = TargetType.Editor;
         ExtraModuleNames.AddRange( new string[] { "AccelByteUe4SdkDemo", "AccelByteUe4Sdk" } );
     }
