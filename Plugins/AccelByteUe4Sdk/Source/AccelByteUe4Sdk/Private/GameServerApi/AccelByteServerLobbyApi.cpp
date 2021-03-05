4a85981803fc6131d644126073620d557372964e
Leonardus Windiono <windio@accelbyte.net>
2021-03-05 07:17:35 +0700

    fix: Remove #pragma once from cpp file, which can cause compilation failures on some platforms.
    https://github.com/AccelByte/accelbyte-ue4-sdk/pull/5

--- Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Private/GameServerApi/AccelByteServerLobbyApi.cpp
+++ Plugins/AccelByteUe4Sdk/Source/AccelByteUe4Sdk/Private/GameServerApi/AccelByteServerLobbyApi.cpp
@@ -2,8 +2,6 @@
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
-#pragma once
-
 #include "GameServerApi/AccelByteServerLobby.h"
 #include "Core/AccelByteRegistry.h"
 #include "Core/AccelByteHttpRetryScheduler.h"
