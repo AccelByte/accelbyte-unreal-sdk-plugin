ef5da76616f7280370ce07dd211199bd402a6d96
Wijaya Adhisurya <wijaya.adhisurya@gmail.com>
2021-07-06 15:07:37 +0700

    chore(jenkins): report to slack channel with list of failed tests

--- Jenkins/Build.ps1
+++ Jenkins/Build.ps1
@@ -2,6 +2,6 @@
 
 & "$Env:ENGINE_PATH\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun `
 -project="$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
--noP4 -nocompile -nocompileeditor -installed `
+-noP4 -nocompile -installed `
 -ue4exe="$Env:ENGINE_PATH\Engine\Binaries\Win64\UE4Editor-Cmd.exe" -utf8output `
--config=Development -platform="$platform" -clean -build -skipcook
+-config=Development -platform="$platform" -build -skipcook
