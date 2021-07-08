2c9deca8fcb2a40490651ae7370dc9fa14d91126
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2021-07-08 09:40:57 +0700

    feat(ue5ea): Jenkins - RunTests.ps1 - add alternate unreal editor exe for UE5 and UE4

--- Jenkins/RunTests.ps1
+++ Jenkins/RunTests.ps1
@@ -1,6 +1,13 @@
 ﻿param([string]$tests, [string]$log, [string]$report)
 
-& "$Env:ENGINE_PATH\Engine\Binaries\Win64\UE4Editor-Cmd.exe" "$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
+$UEC_EXE = "$env:ENGINE_PATH\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" # Default for UE5+
+		
+if (!(Test-Path "$UEC_EXE"))
+{
+	$UEC_EXE = "$env:ENGINE_PATH\Engine\Binaries\Win64\UE4Editor-Cmd.exe" # Fallback for UE4
+}
+
+& "$UEC_EXE" "$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
 -Server -stdout -unattended -nopause `
 -ExecCmds="Automation RunTests $tests; Quit" `
 -log -log="$log" -provider=baremetal -LogCmds="log LogAccelByte verbose" `
