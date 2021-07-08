d93e2d28538a4f646f606568cd9ed042d6ce9f38
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2021-07-08 09:32:22 +0700

    feat(ue5ea): Jenkins - Build.ps1 - remove superficial option ue4exe

--- Jenkins/Build.ps1
+++ Jenkins/Build.ps1
@@ -3,5 +3,5 @@
 & "$Env:ENGINE_PATH\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun `
 -project="$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
 -noP4 -nocompile -installed `
--ue4exe="$Env:ENGINE_PATH\Engine\Binaries\Win64\UE4Editor-Cmd.exe" -utf8output `
+-utf8output `
 -config=Development -platform="$platform" -build -skipcook
