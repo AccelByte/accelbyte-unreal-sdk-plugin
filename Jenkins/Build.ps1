3a98e60f5840eb3c28f4c595a234aa952c71176a
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2021-07-13 15:45:07 +0700

    feat(jenkins): make build linux & windows parallel + separate build editor and game to make ci faster

--- Jenkins/Build.ps1
+++ Jenkins/Build.ps1
@@ -1,7 +1,17 @@
 ﻿param([string]$platform = "Win64")
 
+# Build editor for build host WITHOUT clean to reduce compile time (Win64 only)
+
 & "$Env:ENGINE_PATH\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun `
 -project="$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
--noP4 -nocompile -installed `
+-noP4 -nocompile -editortest -installed `
+-utf8output `
+-config=Development -platform="Win64" -build
+
+# Build game WITH clean to improve consistency (Win64 or Linux)
+
+& "$Env:ENGINE_PATH\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun `
+-project="$Env:WORKSPACE\AccelByteUe4SdkDemo.uproject" `
+-noP4 -nocompile -nocompileeditor -installed -clean `
 -utf8output `
 -config=Development -platform="$platform" -build -skipcook
