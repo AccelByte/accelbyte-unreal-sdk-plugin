25b909a3281b5de4271e55c75d012655a4a84056
diarfs <diar@accelbyte.net>
2021-07-19 14:16:22 +0700

    fix(UGC): Rewrite the inline documentation: fixing from copy-paste mistake, and not standard writing. Fix the copyright.
    (cherry picked from commit 80d49bf68303925e6d25d7373367ccd2c3cc4802)

--- Source/AccelByteUe4Sdk/Public/Api/AccelByteUGCApi.h
+++ Source/AccelByteUe4Sdk/Public/Api/AccelByteUGCApi.h
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2019 - 2021 AccelByte Inc. All Rights Reserved.
+﻿// Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
@@ -17,151 +17,154 @@ class Settings;
 namespace Api
 {
 /**
-* 
+* Provide APIs to access UGC service.
 */
 class ACCELBYTEUE4SDK_API UGC
 {
 
 public:
+
 	UGC(const Credentials& Credentials, const Settings& Settings);
 	~UGC();
 
 private:
+
 	const Credentials& Credentials;
 	const Settings& Settings;
 
 public:
-	/** @brief Create a content and get the payload url to upload the content 
-	 *
-	 * @param ChannelId Save the content into this channel
-	 * @param Request Detail info for the content
-	 * @param OnSuccess Fired when create content is succeed. Returning the detailed info of the Content
-	 * @param OnError Fired when fail
-	 */
-	void CreateContent(const FString& ChannelId, const FAccelByteModelsUGCRequest& CreateRequest, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
-	
-	/** @brief Create a content and get the payload url to upload the content. This is an alternative version, that you can pass as TArray<uint8> for the Preview.
+
+	/** @brief Create a content with FString preview and get the payload url to upload the content.
 	*
-	* @param ChannelId Save the content into this channel
-	* @param Name name of the content
-	* @param Type of the content
-	* @param SubType of the content
-	* @param Tags of the content
-	* @param Preview of the content. It will automatically Encoded into FString
-	* @param FileExtension of the content
-	* @param OnSuccess Fired when create content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ChannelId The id of the content's channel.
+	* @param UGCRequest Detail information for the content request.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCResponse.
+	* @param OnError This will be called when the operation failed.
+	*/
+	void CreateContent(const FString& ChannelId, const FAccelByteModelsUGCRequest& UGCRequest, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
+	
+	/** @brief Create a content with TArray<uint8> Preview paramater and get the payload url to upload the content.
+	*
+	* @param ChannelId The id of the content's channel.
+	* @param Name Name of the content.
+	* @param Type Type of the content.
+	* @param SubType SubType of the content.
+	* @param Tags Tags of the content.
+	* @param Preview The TArray<uint8> of the content's Preview
+	* @param FileExtension FileExtension of the content.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void CreateContent(const FString& ChannelId, const FString& Name, const FString& Type, const FString& SubType, const TArray<FString>& Tags, const TArray<uint8>& Preview,
 						const FString& FileExtension, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
 
-	/** @brief Modify existing content to update some information
+	/** @brief Modify existing content to update some information with FString preview.
 	*
-	* @param ChannelId Save the content into this channel
-	* @param ContentId Content you want to modified
-	* @param ModifyRequest Detail info for the content
-	* @param OnSuccess Fired when Modify content is succeed.
-	* @param OnError Fired when fail
+	* @param ChannelId The id of the content's channel.
+	* @param ContentId The id of the content that will be modified.
+	* @param UGCRequest Detail information for the content request that will be modified.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
-	void ModifyContent(const FString& ChannelId, const FString& ContentId, const FAccelByteModelsUGCRequest& ModifyRequest, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
+	void ModifyContent(const FString& ChannelId, const FString& ContentId, const FAccelByteModelsUGCRequest& UGCRequest, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Modify existing content to update some information
+	/** @brief Modify existing content to update some information with TArray<uint8> Preview paramater.
 	*
-	* @param ChannelId Modify the content from this channel
-	* @param ContentId Content you want to modified
-	* @param Name name of the content
-	* @param Type of the content
-	* @param SubType of the content
-	* @param Tags of the content
-	* @param Preview of the content. It will automatically Encoded into FString
-	* @param FileExtension of the content
-	* @param OnSuccess Fired when Modify content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ChannelId The id of the content's channel.
+	* @param ContentId The id of the content that will be modified.
+	* @param Name Name of the content.
+	* @param Type Type of the content.
+	* @param SubType SubType of the content.
+	* @param Tags Tags of the content.
+	* @param Preview The TArray<uint8> of the content's Preview
+	* @param FileExtension FileExtension of the content.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void ModifyContent(const FString& ChannelId, const FString& ContentId, const FString& Name, const FString& Type, const FString& SubType, const TArray<FString>& Tags, const TArray<uint8>& Preview,
 						const FString& FileExtension, const THandler<FAccelByteModelsUGCResponse>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Modify existing content to update some information
+	/** @brief Delete a content based on the its channel id and content id.
 	*
-	* @param ChannelId Delete the content from this channel
-	* @param ContentId Content you want to deleted
-	* @param OnSuccess Fired when Delete content is succeed.
-	* @param OnError Fired when fail
+	* @param ChannelId The id of the content's channel.
+	* @param ContentId The id of the content that will be deleted.
+	* @param OnSuccess This will be called when the operation succeeded.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void DeleteContent(const FString& ChannelId, const FString& ContentId, const FVoidHandler& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Get Content by Content Id
+	/** @brief Get a content information by its content id
 	*
-	* @param ContentId you want to get
-	* @param OnSuccess Fired when create content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ContentId The id of the content that will be fetched.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCContentResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void GetContentByContentId(const FString& ContentId, const THandler<FAccelByteModelsUGCContentResponse>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Get content by shared code
+	/** @brief Get a content information by its share code.
 	*
-	* @param ShareCode you want to get the content.
-	* @param OnSuccess Fired when Get content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ShareCode The share code of the content that will be fetched.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCContentResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void GetContentByShareCode(const FString& ShareCode, const THandler<FAccelByteModelsUGCContentResponse>& OnSuccess, const FErrorHandler& OnError);
 
-	/** @brief Get content Preview
+	/** @brief Get content Preview as FAccelByteModelsUGCPreview.
 	*
-	* @param ContentId you want to get the content.
-	* @param OnSuccess Fired when Get content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ContentId The id of the Preview's content that will be fetched.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCPreview.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void GetContentPreview(const FString& ContentId, const THandler<FAccelByteModelsUGCPreview>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Get content Preview
+	/** @brief Get content preview as TArray<uint8>.
 	*
-	* @param ContentId you want to get the content.
-	* @param OnSuccess Fired when Get content is succeed. Returning the detailed info of the Content
-	* @param OnError Fired when fail
+	* @param ContentId The id of the Preview's content that will be fetched.
+	* @param OnSuccess This will be called when the operation succeeded. The result is TArray<uint8>.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void GetContentPreview(const FString& ContentId, const THandler<TArray<uint8>>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Get all Tags
+	/** @brief Get all tags.
 	*
-	* @param OnSuccess Fired when Get Tags is succeed. Returning the info of the Tags
-	* @param OnError Fired when fail
-	* @param Limit How many tags you'll get
-	* @param Offset Start from
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCTagsPagingResponse.
+	* @param OnError This will be called when the operation failed.
+	* @param Limit The limit of the tags results. Default value is 1000.
+	* @param Offset The offset of the tags results. Default value is 0.
 	*/
 	void GetTags(const THandler<FAccelByteModelsUGCTagsPagingResponse>& OnSuccess, const FErrorHandler& OnError, int32 Limit = 1000, int32 Offset = 0);
 	
-	/** @brief Get all Types, including SubTypes
+	/** @brief Get all types and subtypes.
 	*
-	* @param OnSuccess Fired when Get Types is succeed. Returning the info of the Types
-	* @param OnError Fired when fail
-	* @param Limit How many types you'll get
-	* @param Offset Start from
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCTypesPagingResponse.
+	* @param OnError This will be called when the operation failed.
+	* @param Limit The limit of the types and subtypes results. Default value is 1000.
+	* @param Offset The offset of the types and subtypes results. Default value is 0.
 	*/
 	void GetTypes(const THandler<FAccelByteModelsUGCTypesPagingResponse>& OnSuccess, const FErrorHandler& OnError, int32 Limit = 1000, int32 Offset = 0);
 
-	/** @brief Create Channel into the user
+	/** @brief Create a player's channel with specific channel name.
 	*
-	* @param ChannelName The name of the channel
-	* @param OnSuccess Fired when create channel is succeed. Returning the detailed info of the channel
-	* @param OnError Fired when fail
+	* @param ChannelName The name of the channel.
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCChannelResponse.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void CreateChannel(const FString& ChannelName, const THandler<FAccelByteModelsUGCChannelResponse>& OnSuccess, const FErrorHandler& OnError);
 	
-	/** @brief Get All Channel related to the user
+	/** @brief Get all of the player's channels.
 	*
-	* @param OnSuccess Fired when create channel is succeed. Returning the detailed info of the channel
-	* @param OnError Fired when fail
-	* @param Limit How many channel you'll get
-	* @param Offset Start from
+	* @param OnSuccess This will be called when the operation succeeded. The result is FAccelByteModelsUGCChannelsPagingResponse.
+	* @param OnError This will be called when the operation failed.
+	* @param Limit The limit of the channel results. Default value is 1000.
+	* @param Offset The offset of the channel results. Default value is 0.
 	*/
 	void GetChannels(const THandler<FAccelByteModelsUGCChannelsPagingResponse>& OnSuccess, const FErrorHandler& OnError, int32 Limit = 1000, int32 Offset = 0);
 
-	/** @brief Delete Channel from the user
+	/** @brief Delete a player's channel based on the its channel id.
 	*
-	* @param ChannelId Channel you want to delete
-	* @param OnSuccess Fired when delete channel is succeed.
-	* @param OnError Fired when fail
+	* @param ChannelId The id of the channel that will be deleted.
+	* @param OnSuccess This will be called when the operation succeeded.
+	* @param OnError This will be called when the operation failed.
 	*/
 	void DeleteChannel(const FString& ChannelId, const FVoidHandler& OnSuccess, const FErrorHandler& OnError);
 	
