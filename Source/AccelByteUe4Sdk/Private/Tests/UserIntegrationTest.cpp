37e7a986670b9120e2bfa30942ce9b226ac335a2
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2021-08-01 06:18:37 +0700

    test(AUser): fix test regression due to invalid partial query result assertion
    this issue was caught in the demo environment daily test.
    partial query can return multiple matches if the partial query term is not unique (not just the expected test user).
    our test result should not be affected by already existing data in the test environment.
    also we are not supposed to test backend behavior here, so we remove those tests

--- Source/AccelByteUe4Sdk/Private/Tests/UserIntegrationTest.cpp
+++ Source/AccelByteUe4Sdk/Private/Tests/UserIntegrationTest.cpp
@@ -2594,20 +2594,6 @@ bool FGetUserByEmailAddressTest::RunTest(const FString& Parameter)
 
 	Waiting(bGetUserDone, "Waiting for Search Users...");
 
-	// Partial email.
-	bool bGetUserPartialQueryDone = false;
-	FPagedPublicUsersInfo ReceivedUserPartialQueryData;
-	FRegistry::User.SearchUsers(
-		EmailAddress.Left(15),
-		THandler<FPagedPublicUsersInfo>::CreateLambda([&bGetUserPartialQueryDone, &ReceivedUserPartialQueryData](const FPagedPublicUsersInfo& Result)
-	{
-		UE_LOG(LogAccelByteUserTest, Log, TEXT("    Success"));
-		ReceivedUserPartialQueryData = Result;
-		bGetUserPartialQueryDone = true;
-	}), UserTestErrorHandler);
-
-	Waiting(bGetUserPartialQueryDone, "Waiting for Search Users...");
-
 #pragma region DeleteUserById
 
 	bool bDeleteDone = false;
@@ -2626,11 +2612,9 @@ bool FGetUserByEmailAddressTest::RunTest(const FString& Parameter)
 
 	AB_TEST_TRUE(bLoginSuccessful);
 	AB_TEST_TRUE(bGetUserDone);
-	AB_TEST_TRUE(bGetUserPartialQueryDone);
 	AB_TEST_TRUE(bDeleteSuccessful);
 	AB_TEST_EQUAL(ReceivedUserData.Data.Num(), 1);
 	AB_TEST_EQUAL(ReceivedUserData.Data[0].DisplayName, DisplayName);
-	AB_TEST_EQUAL(ReceivedUserPartialQueryData.Data.Num(), 0);
 	return true;
 }
 
@@ -2696,22 +2680,6 @@ bool FGetUserByDisplayNameTest::RunTest(const FString& Parameter)
 
 	Waiting(bGetUserDone, "Waiting for Search Users...");
 
-	// Partial DisplayName.
-	bool bGetUserPartialQueryDone = false;
-	FPagedPublicUsersInfo ReceivedUserPartialQueryData;
-	FRegistry::User.SearchUsers(
-		DisplayName.Left(4),
-		EAccelByteSearchType::DISPLAYNAME,
-		THandler<FPagedPublicUsersInfo>::CreateLambda([&bGetUserPartialQueryDone, &ReceivedUserPartialQueryData](const FPagedPublicUsersInfo& Result)
-	{
-		UE_LOG(LogAccelByteUserTest, Log, TEXT("    Success"));
-		ReceivedUserPartialQueryData = Result;
-		bGetUserPartialQueryDone = true;
-	}),
-		UserTestErrorHandler);
-
-	Waiting(bGetUserPartialQueryDone, "Waiting for Search Users...");
-
 #pragma region DeleteUserById
 
 	bool bDeleteDone = false;
@@ -2730,7 +2698,6 @@ bool FGetUserByDisplayNameTest::RunTest(const FString& Parameter)
 
 	AB_TEST_TRUE(bLoginSuccessful);
 	AB_TEST_TRUE(bGetUserDone);
-	AB_TEST_TRUE(bGetUserPartialQueryDone);
 	AB_TEST_TRUE(bDeleteSuccessful);
 	AB_TEST_NOT_EQUAL(ReceivedUserData.Data.Num(), 0);
 	const bool bDisplayNameFound = ReceivedUserData.Data.ContainsByPredicate([DisplayName](const FPublicUserInfo& Item)
@@ -2738,7 +2705,6 @@ bool FGetUserByDisplayNameTest::RunTest(const FString& Parameter)
 			return Item.DisplayName == DisplayName;
 		});
 	AB_TEST_TRUE(bDisplayNameFound);
-	AB_TEST_EQUAL(ReceivedUserPartialQueryData.Data.Num(), 0);
 	return true;
 }
 
@@ -2804,22 +2770,6 @@ bool FGetUserByUsernameTest::RunTest(const FString& Parameter)
 
 	Waiting(bGetUserDone, "Waiting for Search Users...");
 
-	// Partial username.
-	bool bGetUserPartialQueryDone = false;
-	FPagedPublicUsersInfo ReceivedUserPartialQueryData;
-	FRegistry::User.SearchUsers(
-		Username.Left(4),
-		EAccelByteSearchType::USERNAME,
-		THandler<FPagedPublicUsersInfo>::CreateLambda([&bGetUserPartialQueryDone, &ReceivedUserPartialQueryData](const FPagedPublicUsersInfo& Result)
-	{
-		UE_LOG(LogAccelByteUserTest, Log, TEXT("    Success"));
-		ReceivedUserPartialQueryData = Result;
-		bGetUserPartialQueryDone = true;
-	}),
-		UserTestErrorHandler);
-
-	Waiting(bGetUserPartialQueryDone, "Waiting for Search Users...");
-
 #pragma region DeleteUserById
 
 	bool bDeleteDone = false;
@@ -2838,11 +2788,9 @@ bool FGetUserByUsernameTest::RunTest(const FString& Parameter)
 
 	AB_TEST_TRUE(bLoginSuccessful);
 	AB_TEST_TRUE(bGetUserDone);
-	AB_TEST_TRUE(bGetUserPartialQueryDone);
 	AB_TEST_TRUE(bDeleteSuccessful);
 	AB_TEST_EQUAL(ReceivedUserData.Data.Num(), 1);
 	AB_TEST_EQUAL(ReceivedUserData.Data[0].UserName, Username);
-	AB_TEST_EQUAL(ReceivedUserPartialQueryData.Data.Num(), 0);
 	return true;
 }
 
