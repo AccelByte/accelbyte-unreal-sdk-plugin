20008bbb0186aa55d19108fe2079e9d51ad68a23
Narendra (Sonny) Sulaksono <narendra@accelbyte.net>
2021-10-07 14:02:47 +0700

    chore(lobby): Change copyright year

--- Source/AccelByteUe4Sdk/Private/Api/AccelByteLobbyApi.cpp
+++ Source/AccelByteUe4Sdk/Private/Api/AccelByteLobbyApi.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 - 2020 AccelByte Inc. All Rights Reserved.
+// Copyright (c) 2018 - 2021 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
