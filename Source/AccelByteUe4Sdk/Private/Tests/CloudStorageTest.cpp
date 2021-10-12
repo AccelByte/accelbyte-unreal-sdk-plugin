5840fe96735618e7194c21b998e8b95f0b27551c
arya <arya@accelbyte.net>
2021-10-12 13:40:49 +0700

    chore(test): improve CloudStorage and MultiRegistry tests
    *revise statistic test copyright to 2019-2021

--- Source/AccelByteUe4Sdk/Private/Tests/CloudStorageTest.cpp
+++ Source/AccelByteUe4Sdk/Private/Tests/CloudStorageTest.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2019 AccelByte Inc. All Rights Reserved.
+// Copyright (c) 2018-2021 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
@@ -15,215 +15,160 @@ using AccelByte::FErrorHandler;
 using AccelByte::Credentials;
 using AccelByte::HandleHttpError;
 using AccelByte::Api::User;
-using AccelByte::Api::CloudStorage;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogAccelByteCloudStorageTest, Log, All);
 DEFINE_LOG_CATEGORY(LogAccelByteCloudStorageTest);
 
 const int32 AutomationFlagMaskCloudStorage = (EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::CommandletContext | EAutomationTestFlags::ClientContext);
 
-FString Payloads[2] = { TEXT("PayloadOne"), TEXT("PayloadTwo") };
-FString OriginalNames[2] = { TEXT("File1.txt"), TEXT("File2.txt") };
-TArray<FString> Tags = { TEXT("Tag1"), TEXT("Tag2") };
-TArray<FString> UpdateTags = { TEXT("Tag3"), TEXT("Tag4") };
-FString Labels[2] = { TEXT("Label1"), TEXT("Label2") };
-FAccelByteModelsSlot CreatedSlot;
-
 const auto CloudStorageErrorHandler = FErrorHandler::CreateLambda([](int32 ErrorCode, FString ErrorMessage)
 {
 	UE_LOG(LogAccelByteCloudStorageTest, Error, TEXT("Error code: %d\nError message:%s"), ErrorCode, *ErrorMessage);
 });
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageSetup, "AccelByte.Tests.CloudStorage.A.Setup", AutomationFlagMaskCloudStorage);
-bool CloudStorageSetup::RunTest(const FString& Parameters)
+BEGIN_DEFINE_SPEC(FCloudStorageTest, "AccelByte.Tests.CloudStorage", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+	FTestUser TestUser;
+	FString Payloads[2] = { TEXT("PayloadOne"), TEXT("PayloadTwo") };
+	FString OriginalNames[2] = { TEXT("File1.txt"), TEXT("File2.txt") };
+	TArray<FString> Tags = { TEXT("Tag1"), TEXT("Tag2") };
+	TArray<FString> UpdateTags = { TEXT("Tag3"), TEXT("Tag4") };
+	FString Labels[2] = { TEXT("Label1"), TEXT("Label2") };
+	FAccelByteModelsSlot CreatedSlot;
+END_DEFINE_SPEC(FCloudStorageTest)
+
+void FCloudStorageTest::Define()
 {
-
-	bool bUserLoginResult = false;
-	FRegistry::User.LoginWithDeviceId(FVoidHandler::CreateLambda([&bUserLoginResult]()
+	const auto setupOnce = [this]()
 	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("User Login Success"));
-		bUserLoginResult = true;
-	}), CloudStorageErrorHandler);
-	WaitUntil(bUserLoginResult, "Waiting for Login...");
+		AB_TEST_TRUE(RegisterTestUser(TestUser));
+		AB_TEST_TRUE(LoginTestUser(TestUser));
+		
+		return true;
+	};
 
-	AB_TEST_TRUE(bUserLoginResult);
-	return true;
-}
+	BeforeEach(setupOnce);
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageTearDown, "AccelByte.Tests.CloudStorage.Z.TearDown", AutomationFlagMaskCloudStorage);
-bool CloudStorageTearDown::RunTest(const FString& Parameters)
-{
-	bool bDeleteDone = false;
-	bool bDeleteSuccessful = false;
-	
-	UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("DeleteUserById"));
-	AdminDeleteUser(FRegistry::Credentials.GetUserId(), FSimpleDelegate::CreateLambda([&bDeleteDone, &bDeleteSuccessful]()
+	const auto teardown = [this]()
+	{		
+		AB_TEST_TRUE(DeleteTestUser(TestUser));
+
+		return true;
+	};
+
+	AfterEach(teardown);
+
+	Describe("Create_Update_Get_Delete_Slot", [this]()
 	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("    Success"));
-		bDeleteSuccessful = true;
-		bDeleteDone = true;
-	}), CloudStorageErrorHandler);
-	WaitUntil(bDeleteDone, "Waiting for deletion...");
-	
-	AB_TEST_TRUE(bDeleteSuccessful);
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageCreateSlot, "AccelByte.Tests.CloudStorage.B.CreateSlot", AutomationFlagMaskCloudStorage);
-bool CloudStorageCreateSlot::RunTest(const FString& Parameters)
-{
-	bool bSlotCreatedResult = false;
-	FRegistry::CloudStorage.CreateSlot(UAccelByteBlueprintsTest::FStringToBytes(Payloads[0]), "create.txt", Tags, Labels[0], "customAttributeCreated", THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
-	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Create Slot Success"));
-		CreatedSlot = Result;
-		bSlotCreatedResult = true;
-	}), nullptr, CloudStorageErrorHandler);
-	WaitUntil(bSlotCreatedResult, "Waiting for slot created...");
-	
-	AB_TEST_TRUE(bSlotCreatedResult);
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageUpdateSlot, "AccelByte.Tests.CloudStorage.C.UpdateSlot", AutomationFlagMaskCloudStorage);
-bool CloudStorageUpdateSlot::RunTest(const FString& Parameters)
-{
-	bool bSlotUpdatedResult = false;
-	FRegistry::CloudStorage.UpdateSlot(CreatedSlot.SlotId, UAccelByteBlueprintsTest::FStringToBytes(Payloads[1]), "update.txt", UpdateTags, Labels[1], "customAttributeUpdated", THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
-	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Update Slot Success"));
-		bSlotUpdatedResult = true;
-	}), nullptr, CloudStorageErrorHandler);
-	WaitUntil(bSlotUpdatedResult, "Waiting for slot updated...");
-	
-	AB_TEST_TRUE(bSlotUpdatedResult);
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageUpdateMetadataSlot, "AccelByte.Tests.CloudStorage.C.UpdateMetadata", AutomationFlagMaskCloudStorage);
-bool CloudStorageUpdateMetadataSlot::RunTest(const FString& Parameters)
-{
-	UpdateTags[0] = (TEXT("tagOne"));
-	UpdateTags[1] = (TEXT("tagTwo"));
-	FString UpdateLabel = "metadataLabel";
-	FString UpdateCustomAttribute = TEXT("This is a custom attribute");
-
-	bool bMetadataUpdatedResult = false;
-	FRegistry::CloudStorage.UpdateSlotMetadata(CreatedSlot.SlotId, UpdateTags, UpdateLabel, UpdateCustomAttribute, THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
-	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Update Metadata Success"));
-		bMetadataUpdatedResult = true;
-	}), nullptr, CloudStorageErrorHandler);
-	WaitUntil(bMetadataUpdatedResult, "Waiting for meta updated...");
-	AB_TEST_TRUE(bMetadataUpdatedResult);
-
-	bool bGetSlotAfterUpdateResult = false;
-	FRegistry::CloudStorage.GetAllSlots(THandler<TArray<FAccelByteModelsSlot>>::CreateLambda([&](TArray<FAccelByteModelsSlot> Result)
-	{
-		for (int i = 0; i < Result.Num(); i++)
+		It("Should run CRUD test then return true", [this]()
 		{
-			if (Result[i].SlotId == CreatedSlot.SlotId)
+			Api::CloudStorage CloudStorage(TestUser.Credentials, FRegistry::Settings);
+			
+			bool bSlotCreatedResult = false;
+			CloudStorage.CreateSlot(UAccelByteBlueprintsTest::FStringToBytes(Payloads[0]), "create.txt", Tags, Labels[0], "customAttributeCreated",
+				THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
 			{
-				if (Result[i].Label != UpdateLabel)
+				UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Create Slot Success"));
+				CreatedSlot = Result;
+				bSlotCreatedResult = true;
+			}), nullptr, CloudStorageErrorHandler);
+
+			WaitUntil(bSlotCreatedResult, "Waiting for slot created...");
+			AB_TEST_TRUE(bSlotCreatedResult);
+			
+			UpdateTags[0] = (TEXT("tagOne"));
+			UpdateTags[1] = (TEXT("tagTwo"));
+			FString UpdateLabel = "metadataLabel";
+			FString UpdateCustomAttribute = TEXT("This is a custom attribute");
+
+			bool bMetadataUpdatedResult = false;
+			CloudStorage.UpdateSlotMetadata(CreatedSlot.SlotId, UpdateTags, UpdateLabel, UpdateCustomAttribute, THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
+			{
+				UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Update Metadata Success"));
+				bMetadataUpdatedResult = true;
+			}), nullptr, CloudStorageErrorHandler);
+			WaitUntil(bMetadataUpdatedResult, "Waiting for meta updated...");
+			AB_TEST_TRUE(bMetadataUpdatedResult);
+
+			bool bGetSlotAfterUpdateResult = false;
+			CloudStorage.GetAllSlots(THandler<TArray<FAccelByteModelsSlot>>::CreateLambda([&](TArray<FAccelByteModelsSlot> Result)
+			{
+				for (int i = 0; i < Result.Num(); i++)
 				{
-					break;
+					if (Result[i].SlotId == CreatedSlot.SlotId)
+					{
+						if (Result[i].Label != UpdateLabel)
+						{
+							break;
+						}
+						if (!Result[i].Tags.Contains(UpdateTags[0]))
+						{
+							break;
+						}
+						if (!Result[i].Tags.Contains(UpdateTags[1]))
+						{
+							break;
+						}
+						if (Result[i].CustomAttribute != UpdateCustomAttribute)
+						{
+							break;
+						}
+						bGetSlotAfterUpdateResult = true;
+					}
 				}
-				if (!Result[i].Tags.Contains(UpdateTags[0]))
+			}), CloudStorageErrorHandler);
+			WaitUntil(bGetSlotAfterUpdateResult, "Waiting for get slot...");
+			AB_TEST_TRUE(bGetSlotAfterUpdateResult);
+
+			bool bSlotUpdatedResult = false;
+			CloudStorage.UpdateSlot(CreatedSlot.SlotId, UAccelByteBlueprintsTest::FStringToBytes(Payloads[1]), "update.txt", UpdateTags, Labels[1], "customAttributeUpdated", THandler<FAccelByteModelsSlot>::CreateLambda([&](const FAccelByteModelsSlot& Result)
+			{
+				UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Update Slot Success"));
+				bSlotUpdatedResult = true;
+			}), nullptr, CloudStorageErrorHandler);
+
+			WaitUntil(bSlotUpdatedResult, "Waiting for slot updated...");
+			AB_TEST_TRUE(bSlotUpdatedResult);
+
+			bool bGetSlotResult = false;
+			bool bSlotContentUpdated = false;
+			CloudStorage.GetSlot(CreatedSlot.SlotId, THandler<TArray<uint8>>::CreateLambda([&](const TArray<uint8>& Data)
+			{
+				bGetSlotResult = true;
+				bSlotContentUpdated = (UAccelByteBlueprintsTest::BytesToFString(Data) == Payloads[1]);
+			}), CloudStorageErrorHandler);
+			WaitUntil(bGetSlotResult, "Waiting for get slot...");
+
+			AB_TEST_TRUE(bGetSlotResult);
+			
+
+			bool bGetAllSlotsResult = false;
+			TArray<FAccelByteModelsSlot> Results;
+			CloudStorage.GetAllSlots(THandler<TArray<FAccelByteModelsSlot>>::CreateLambda([&](const TArray<FAccelByteModelsSlot>& Slots)
+			{
+				UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Get All Slots Success"));
+				Results = Slots;
+				bGetAllSlotsResult = true;
+			}), CloudStorageErrorHandler);
+			WaitUntil(bGetAllSlotsResult, "Waiting for get all slot...");
+			AB_TEST_TRUE(bGetAllSlotsResult);
+
+			TArray<bool> bDeleteSlotResults;
+			for (int i = 0; i < Results.Num(); i++)
+			{
+				bDeleteSlotResults.Add(false);
+				CloudStorage.DeleteSlot(Results[i].SlotId, FVoidHandler::CreateLambda([&]()
 				{
-					break;
-				}
-				if (!Result[i].Tags.Contains(UpdateTags[1]))
+					UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Delete Slot %d / %d Success"), i+1, Results.Num());
+					bDeleteSlotResults[i] = true;
+				}), FErrorHandler::CreateLambda([&](int32 Code, FString Message)
 				{
-					break;
-				}
-				if (Result[i].CustomAttribute != UpdateCustomAttribute)
-				{
-					break;
-				}
-				bGetSlotAfterUpdateResult = true;
+					bDeleteSlotResults[i] = false;
+				}));
+				WaitUntil(bDeleteSlotResults[i], "Waiting for slot deletion...");
 			}
-		}
-	}), CloudStorageErrorHandler);
-	WaitUntil(bGetSlotAfterUpdateResult, "Waiting for get slot...");
-	AB_TEST_TRUE(bGetSlotAfterUpdateResult);
+			AB_TEST_FALSE(bDeleteSlotResults.Contains(false));
 
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageGetAllSlots, "AccelByte.Tests.CloudStorage.D.GetAllSlots", AutomationFlagMaskCloudStorage);
-bool CloudStorageGetAllSlots::RunTest(const FString& Parameter)
-{
-	bool bGetAllSlotsResult = false;
-	TArray<FAccelByteModelsSlot> Results;
-	FRegistry::CloudStorage.GetAllSlots(THandler<TArray<FAccelByteModelsSlot>>::CreateLambda([&](const TArray<FAccelByteModelsSlot>& Slots)
-	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Get All Slots Success"));
-		Results = Slots;
-		bGetAllSlotsResult = true;
-	}), CloudStorageErrorHandler);
-	WaitUntil(bGetAllSlotsResult, "Waiting for get all slot...");
-
-	bool bUpdatedSlotFound = false;
-	for (int i = 0; i < Results.Num(); i++)
-	{
-		if (Results[i].SlotId == CreatedSlot.SlotId)
-		{
-			bUpdatedSlotFound = true;
-		}
-	}
-
-	AB_TEST_TRUE(bGetAllSlotsResult);
-	AB_TEST_TRUE(Results.Num() > 0);
-	AB_TEST_TRUE(bUpdatedSlotFound);
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageGetSlot, "AccelByte.Tests.CloudStorage.E.GetSlot", AutomationFlagMaskCloudStorage);
-bool CloudStorageGetSlot::RunTest(const FString& Parameters)
-{
-	bool bGetSlotResult = false;
-	bool bSlotContentUpdated = false;
-	FRegistry::CloudStorage.GetSlot(CreatedSlot.SlotId, THandler<TArray<uint8>>::CreateLambda([&](const TArray<uint8>& Data)
-	{
-		bGetSlotResult = true;
-		bSlotContentUpdated = (UAccelByteBlueprintsTest::BytesToFString(Data) == Payloads[1]);
-	}), CloudStorageErrorHandler);
-	WaitUntil(bGetSlotResult, "Waiting for get slot...");
-
-	AB_TEST_TRUE(bGetSlotResult);
-	AB_TEST_TRUE(bSlotContentUpdated);
-	return true;
-}
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(CloudStorageDeleteSlot, "AccelByte.Tests.CloudStorage.F.DeleteSlot", AutomationFlagMaskCloudStorage);
-bool CloudStorageDeleteSlot::RunTest(const FString& Parameters)
-{
-	bool bGetAllSlotsResult = false;
-	TArray<FAccelByteModelsSlot> Results;
-	FRegistry::CloudStorage.GetAllSlots(THandler<TArray<FAccelByteModelsSlot>>::CreateLambda([&](const TArray<FAccelByteModelsSlot>& Slots)
-	{
-		UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Get All Slots Success"));
-		Results = Slots;
-		bGetAllSlotsResult = true;
-	}), CloudStorageErrorHandler);
-	WaitUntil(bGetAllSlotsResult, "Waiting for get all slot...");
-	AB_TEST_TRUE(bGetAllSlotsResult);
-
-	TArray<bool> bDeleteSlotResults;
-	for (int i = 0; i < Results.Num(); i++)
-	{
-		bDeleteSlotResults.Add(false);
-		FRegistry::CloudStorage.DeleteSlot(Results[i].SlotId, FVoidHandler::CreateLambda([&]()
-		{
-			UE_LOG(LogAccelByteCloudStorageTest, Log, TEXT("Delete Slot %d / %d Success"), i+1, Results.Num());
-			bDeleteSlotResults[i] = true;
-		}), FErrorHandler::CreateLambda([&](int32 Code, FString Message)
-		{
-			bDeleteSlotResults[i] = false;
-		}));
-		WaitUntil(bDeleteSlotResults[i], "Waiting for slot deletion...");
-	}
-	AB_TEST_FALSE(bDeleteSlotResults.Contains(false));
-
-	return true;
+			return true;
+		});
+	});
 }
--- Source/AccelByteUe4Sdk/Private/Tests/MultiRegistryTest.cpp
+++ Source/AccelByteUe4Sdk/Private/Tests/MultiRegistryTest.cpp
@@ -15,99 +15,111 @@ using AccelByte::Api::User;
 DECLARE_LOG_CATEGORY_EXTERN(LogAccelByteMultiRegistryTest, Log, All);
 DEFINE_LOG_CATEGORY(LogAccelByteMultiRegistryTest);
 
-const int32 AutomationFlagMaskMultiRegistry = (
-	EAutomationTestFlags::EditorContext |
-	EAutomationTestFlags::ProductFilter |
-	EAutomationTestFlags::CommandletContext |
-	EAutomationTestFlags::ClientContext);
+const int32 AutomationFlagMaskMultiRegistry = (	EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter |	EAutomationTestFlags::CommandletContext | EAutomationTestFlags::ClientContext);
 
 const auto MultiRegistryTestErrorHandler = FErrorHandler::CreateLambda([](int32 ErrorCode, FString ErrorMessage)
-	{
-		UE_LOG(LogAccelByteMultiRegistryTest, Error, TEXT("Error code: %d\nError message:%s"), ErrorCode, *ErrorMessage);
-	});
-
-TArray<FTestUser> MultiRegistryTestUsers;
-TArray<FCredentialsPtr> MultiRegistryTestCredentials;
-
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMultiRegistryTestLogin, "AccelByte.Tests.MultiRegistry.User", AutomationFlagMaskMultiRegistry);
-bool FMultiRegistryTestLogin::RunTest(const FString& Parameters)
 {
-	// Setup
+	UE_LOG(LogAccelByteMultiRegistryTest, Error, TEXT("Error code: %d\nError message:%s"), ErrorCode, *ErrorMessage);
+});
 
-	AB_TEST_TRUE(SetupTestUsers(2, MultiRegistryTestUsers));
+BEGIN_DEFINE_SPEC(FMultiRegistryTest, "AccelByte.Tests.MultiRegistry", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+	TArray<FTestUser> MultiRegistryTestUsers;
+	TArray<FCredentialsPtr> MultiRegistryTestCredentials;
+END_DEFINE_SPEC(FMultiRegistryTest)
 
-	// Multi user login using FMultiRegistry and FApiClient
-
-	for (const auto& User : MultiRegistryTestUsers)
+void FMultiRegistryTest::Define()
+{
+	const auto setupOnce = [this]()
 	{
-		bool bIsDone = false;
-		bool bIsOk = false;
-		UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("%s: %s"), TEXT("Logging in"), *User.Email);
-		FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(User.Email); // Using email as key
-		ApiClient->User.LoginWithUsername(User.Email, User.Password,
-			FVoidHandler::CreateLambda([&]()
-				{
-					UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("OK"));
-					bIsOk = true;
-					bIsDone = true;
-				}),
-			FErrorHandler::CreateLambda([&](int32 ErrorCode, FString ErrorMessage)
-				{
-					MultiRegistryTestErrorHandler.ExecuteIfBound(ErrorCode, ErrorMessage);
-					bIsDone = true;
-				}));
-		WaitUntil(bIsDone, "Waiting ...");
-		AB_TEST_TRUE(bIsOk);
-	}
+		AB_TEST_TRUE(SetupTestUsers(2, MultiRegistryTestUsers));
+		
+		return true;
+	};
 
-	// Check and load multi user credentials to an array (for testing purposes only)
+	BeforeEach(setupOnce);
 
-	MultiRegistryTestCredentials.Empty();
+	const auto teardown = [this]()
+	{		
+		AB_TEST_TRUE(TeardownTestUsers(MultiRegistryTestUsers));
 
-	TSet<FString> UniqueUserIds;
+		return true;
+	};
 
-	for (const auto& User : MultiRegistryTestUsers)
+	AfterEach(teardown);
+
+	Describe("User", [this]()
 	{
-		FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(User.Email);
-		FString UserId = ApiClient->CredentialsRef->GetUserId();
-		AB_TEST_TRUE(!UserId.IsEmpty());
-		UniqueUserIds.Add(UserId);
-		MultiRegistryTestCredentials.Add(ApiClient->CredentialsRef); 
-	}
+		It("Should perform multi user login using FMultiRegistry and FApiClient then return true", [this]()
+		{
+			// Multi user login
+			for (const auto& User : MultiRegistryTestUsers)
+			{
+				bool bIsDone = false;
+				bool bIsOk = false;
+				UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("%s: %s"), TEXT("Logging in"), *User.Email);
+				FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(User.Email); // Using email as key
+				ApiClient->User.LoginWithUsername(User.Email, User.Password,
+					FVoidHandler::CreateLambda([&]()
+						{
+							UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("OK"));
+							bIsOk = true;
+							bIsDone = true;
+						}),
+					FErrorHandler::CreateLambda([&](int32 ErrorCode, FString ErrorMessage)
+						{
+							MultiRegistryTestErrorHandler.ExecuteIfBound(ErrorCode, ErrorMessage);
+							bIsDone = true;
+						}));
+				WaitUntil(bIsDone, "Waiting ...");
+				AB_TEST_TRUE(bIsOk);
+			}
 
-	AB_TEST_EQUAL(MultiRegistryTestUsers.Num(), UniqueUserIds.Num());
-	AB_TEST_EQUAL(MultiRegistryTestUsers.Num(), MultiRegistryTestCredentials.Num());
+			// Check and load multi user credentials to an array (for testing purposes only)
 
-	// Multi user API GetData using FMultiRegistry and FApiClient
+			MultiRegistryTestCredentials.Empty();
 
-	for (int i = 0; i < MultiRegistryTestUsers.Num(); i++)
-	{
-		FAccountUserData AccountUserData;
-		bool bIsDone = false;
-		bool bIsOk = false;
-		UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("%s: %s"), TEXT("Getting user data"), *MultiRegistryTestUsers[i].Email);
-		FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(MultiRegistryTestUsers[i].Email);
-		ApiClient->User.GetData(
-			THandler<FAccountUserData>::CreateLambda([&](const FAccountUserData& Result)
-				{
-					UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("OK"));
-					AccountUserData = Result;
-					bIsOk = true;
-					bIsDone = true;
-				}),
-			FErrorHandler::CreateLambda([&](int32 ErrorCode, FString ErrorMessage)
-				{
-					MultiRegistryTestErrorHandler.ExecuteIfBound(ErrorCode, ErrorMessage);
-					bIsDone = true;
-				}));
-		WaitUntil(bIsDone, TEXT("Waiting ..."));
-		AB_TEST_TRUE(bIsOk);
-		AB_TEST_EQUAL(AccountUserData.UserId, MultiRegistryTestCredentials[i]->GetUserId());
-	}
+			TSet<FString> UniqueUserIds;
 
-	// Tear down
+			for (const auto& User : MultiRegistryTestUsers)
+			{
+				FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(User.Email);
+				FString UserId = ApiClient->CredentialsRef->GetUserId();
+				AB_TEST_TRUE(!UserId.IsEmpty());
+				UniqueUserIds.Add(UserId);
+				MultiRegistryTestCredentials.Add(ApiClient->CredentialsRef); 
+			}
 
-	AB_TEST_TRUE(TeardownTestUsers(MultiRegistryTestUsers));
+			AB_TEST_EQUAL(MultiRegistryTestUsers.Num(), UniqueUserIds.Num());
+			AB_TEST_EQUAL(MultiRegistryTestUsers.Num(), MultiRegistryTestCredentials.Num());
 
-	return true;
+			// Multi user API GetData using FMultiRegistry and FApiClient
+
+			for (int i = 0; i < MultiRegistryTestUsers.Num(); i++)
+			{
+				FAccountUserData AccountUserData;
+				bool bIsDone = false;
+				bool bIsOk = false;
+				UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("%s: %s"), TEXT("Getting user data"), *MultiRegistryTestUsers[i].Email);
+				FApiClientPtr ApiClient = FMultiRegistry::GetApiClient(MultiRegistryTestUsers[i].Email);
+				ApiClient->User.GetData(
+					THandler<FAccountUserData>::CreateLambda([&](const FAccountUserData& Result)
+						{
+							UE_LOG(LogAccelByteMultiRegistryTest, Log, TEXT("OK"));
+							AccountUserData = Result;
+							bIsOk = true;
+							bIsDone = true;
+						}),
+					FErrorHandler::CreateLambda([&](int32 ErrorCode, FString ErrorMessage)
+						{
+							MultiRegistryTestErrorHandler.ExecuteIfBound(ErrorCode, ErrorMessage);
+							bIsDone = true;
+						}));
+				WaitUntil(bIsDone, TEXT("Waiting ..."));
+				AB_TEST_TRUE(bIsOk);
+				AB_TEST_EQUAL(AccountUserData.UserId, MultiRegistryTestCredentials[i]->GetUserId());
+			}
+			
+			return true;
+		});
+	});
 }
