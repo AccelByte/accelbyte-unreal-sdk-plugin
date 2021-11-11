75a5df7e3309dce951b30efc80df809d97c0fe4e
Narendra (Sonny) Sulaksono <narendra@accelbyte.net>
2021-11-11 08:57:20 +0700

    feat(TokenGenerator): add EntitlementTokenGenerator

---
+++ Source/AccelByteUe4Sdk/Public/Core/AccelByteEntitlementTokenGenerator.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "AccelByteRegistry.h"
+#include "IAccelByteTokenGenerator.h"
+
+namespace AccelByte
+{
+	
+struct FTokenGeneratorParams
+{
+	TArray<FString> ItemIds;
+	TArray<FString> AppIds;
+	TArray<FString> Skus;
+};
+	
+class ACCELBYTEUE4SDK_API FAccelByteEntitlementTokenGenerator : public IAccelByteTokenGenerator
+{
+	DECLARE_DERIVED_EVENT(FAccelByteEntitlementTokenGenerator, IAccelByteTokenGenerator::FTokenReceivedEvent, FTokenReceivedEvent);
+
+public:
+	FAccelByteEntitlementTokenGenerator(const FTokenGeneratorParams& Params);
+	virtual ~FAccelByteEntitlementTokenGenerator() override;
+	
+	virtual void RequestToken() override;
+	virtual bool IsTokenValid() const override;
+	virtual FTokenReceivedEvent& OnTokenReceived() override;
+
+protected:
+	FTokenReceivedEvent TokenReceivedEvent;
+
+private:
+	FDateTime LastSuccessDateTime;
+	FDateTime ExpiredDateTime;
+
+	TArray<FString> Skus;
+	TArray<FString> ItemIds;
+	TArray<FString> AppIds;
+};	
+}
