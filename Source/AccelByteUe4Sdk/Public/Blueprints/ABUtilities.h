24ff88e4590ec9605bd937b14ada944809e02d75
TeoFerdian <teoderikus@accelbyte.net>
2022-07-12 14:12:06 +0700

    feat(blueprints): add utilities to get and modify fjsonobjectwrapper

--- Source/AccelByteUe4Sdk/Public/Blueprints/ABUtilities.h
+++ Source/AccelByteUe4Sdk/Public/Blueprints/ABUtilities.h
@@ -6,6 +6,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "JsonObjectWrapper.h"
 #include "ABUtilities.generated.h"
 
 UCLASS(Blueprintable, BlueprintType)
@@ -15,4 +16,28 @@ class UABUtilities : public UObject
 public:
 	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
 		static FString DateTimeToString(const FDateTime& DateTime);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+	static bool GetJsonValueAsString(const FJsonObjectWrapper& ObjectWrapper, const FString& Key, FString& OutString);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static bool GetJsonValueAsNumber(const FJsonObjectWrapper& ObjectWrapper, const FString& Key, float& OutNumber);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static bool GetJsonValueAsBool(const FJsonObjectWrapper& ObjectWrapper, const FString& Key, bool& OutBool);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static bool GetJsonValueAsArray(const FJsonObjectWrapper& ObjectWrapper, const FString& Key, TArray<FString>& OutArray);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static void AddOrReplaceJsonStringValue(FJsonObjectWrapper& ObjectWrapper, const FString& Key, const FString& Value);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static void AddOrReplaceJsonNumberValue(FJsonObjectWrapper& ObjectWrapper, const FString& Key, float Value);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static void AddOrReplaceJsonBoolValue(FJsonObjectWrapper& ObjectWrapper, const FString& Key, bool Value);
+
+	UFUNCTION(BlueprintCallable, Category = "AccelByte | Utilities")
+		static void AddOrReplaceJsonArrayValue(FJsonObjectWrapper& ObjectWrapper, const FString& Key, const TArray<FString>& Value);
 };
