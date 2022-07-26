530eb6a8fdb414c62a94bc4c7bbeb798209abeb1
Narendra (Sonny) Sulaksono <narendra@accelbyte.net>
2022-07-26 21:45:43 +0700

    chore(MMv2): Add missing function comment in matchmaking v2

--- Source/AccelByteUe4Sdk/Public/Api/AccelByteMatchmakingV2Api.h
+++ Source/AccelByteUe4Sdk/Public/Api/AccelByteMatchmakingV2Api.h
@@ -23,17 +23,40 @@ public:
 	MatchmakingV2(Credentials const& InCredentialsRef, Settings const& InSettingRef, FHttpRetryScheduler& InHttpRef);
 	~MatchmakingV2();
 
+	/**
+	 * @brief Create a matchmaking ticket, this will start matchmaking process.
+	 *
+	 * @param MatchPool Name of the match pool we want to matchmake into.
+	 * @param OnSuccess This will be called if the operation succeeded.
+	 * @param OnError This will be called if the operation failed.
+	 * @param Optionals optional variables we can set for matchmaking process.
+	 */
 	void CreateMatchTicket(
 		const FString& MatchPool,
 		const THandler<FAccelByteModelsV2MatchmakingCreateTicketResponse>& OnSuccess,
 		const FErrorHandler& OnError,
 		const FAccelByteModelsV2MatchTicketOptionalParams& Optionals = {});
-	
+
+	/**
+	 * @brief Get the match ticket details, will get the status of match ticket if it's already matched
+	 * and it's corresponding session ID if already matched into a session.
+	 *
+	 * @param TicketId ID of the matchmaking ticket.
+	 * @param OnSuccess This will be called if the operation succeeded.
+	 * @param OnError This will be called if the operation failed.
+	 */
 	void GetMatchTicketDetails(
 		const FString& TicketId,
 		const THandler<FAccelByteModelsV2MatchmakingGetTicketDetailsResponse>& OnSuccess,
 		const FErrorHandler& OnError);
-	
+
+	/**
+	 * @brief Delete the match ticket. Will cancel the matchmaking process on success.
+	 *
+	 * @param TicketId ID of the matchmaking ticket.
+	 * @param OnSuccess This will be called if the operation succeeded.
+	 * @param OnError This will be called if the operation failed.
+	 */
 	void DeleteMatchTicket(
 	const FString& TicketId,
 	const FVoidHandler& OnSuccess,
