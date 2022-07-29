72ca5909ec20f31d1f5f18dbd633e59f50c0ab6c
Jordan Fitzgerald <jordan@accelbyte.net>
2022-07-29 17:34:37 +0000

    Merged in feature/v2-game-session-complex-query (pull request #365)
    add complex query builder for game sessions
    
    * add complex query builder for game sessions
    
    * Merge branch 'feature/multiplayer-v2' into feature/v2-game-session-complex-query
    
    
    Approved-by: Maxwell Flynn

--- Source/AccelByteUe4Sdk/Public/Models/AccelByteSessionModels.h
+++ Source/AccelByteUe4Sdk/Public/Models/AccelByteSessionModels.h
@@ -8,6 +8,7 @@
 #include "JsonObjectWrapper.h"
 #include "AccelByteDSMModels.h"
 #include "AccelByteGeneralModels.h"
+#include "Core/AccelByteReport.h"
 #include "AccelByteSessionModels.generated.h"
 
 UENUM(BlueprintType)
@@ -48,6 +49,19 @@ enum class EAccelByteV2SessionConfigurationServerType : uint8
 	NONE = 0,
 	DS,
 	P2P
+};
+
+UENUM(BlueprintType)
+enum class EAccelByteV2SessionQueryComparisonOp : uint8
+{
+	EQUAL = 0,
+	NOT_EQUAL,
+	GREATER_THAN,
+	GREATER_THAN_EQUAL,
+	LESS_THAN,
+	LESS_THAN_EQUAL,
+	CONTAINS,
+	NOT_CONTAINS
 };
 
 USTRUCT(BlueprintType)
@@ -304,22 +318,158 @@ struct ACCELBYTEUE4SDK_API FAccelByteModelsV2GameSessionQuery
 
 	FAccelByteModelsV2GameSessionQuery()
 	{
-		FJsonObject JsonObject;
-		JsonWrapper.JsonObject = MakeShared<FJsonObject>(JsonObject);
+		JsonWrapper.JsonObject = MakeShared<FJsonObject>();
 	}
 
-	void AddParam(const FString& Field, const FString& Value) const
+// Macro to reduce code duplication for setting query fields in the JSON wrapper
+#define SET_SESSION_QUERY_FIELD(JsonType, Value)                                                                       \
+	FString ComparisonFieldName;                                                                                       \
+	const TSharedPtr<FJsonObject> ComparisonField = InitializeComparisonField(Comparison, ComparisonFieldName);        \
+	ComparisonField->Set##JsonType##Field(AttributeName, Value);                                                       \
+	AddToQuery(ComparisonFieldName, ComparisonField);                                                                  \
+
+	void AddParam(
+		const FString& AttributeName,
+		const EAccelByteV2SessionQueryComparisonOp Comparison,
+		const FString& AttributeValue) const
 	{
-		JsonWrapper.JsonObject->SetStringField(Field, Value);
+		if(Comparison != EAccelByteV2SessionQueryComparisonOp::EQUAL &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::NOT_EQUAL)
+		{
+			UE_LOG(LogAccelByte, Warning, TEXT("Invalid session query operator/type combination!"));
+			return;
+		}
+
+		SET_SESSION_QUERY_FIELD(String, AttributeValue);
 	}
 
-	void AddParam(const FString& Field, float Value) const
+	void AddParam(
+		const FString& AttributeName,
+		const EAccelByteV2SessionQueryComparisonOp Comparison,
+		const double AttributeValue) const
 	{
-		JsonWrapper.JsonObject->SetNumberField(Field, Value);
+		if(Comparison != EAccelByteV2SessionQueryComparisonOp::EQUAL &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::NOT_EQUAL &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::GREATER_THAN &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::GREATER_THAN_EQUAL &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::LESS_THAN &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::LESS_THAN_EQUAL)
+		{
+			UE_LOG(LogAccelByte, Warning, TEXT("Invalid session query operator/type combination!"));
+			return;
+		}
+
+		SET_SESSION_QUERY_FIELD(Number, AttributeValue);
 	}
+
+	void AddParam(
+		const FString& AttributeName,
+		const EAccelByteV2SessionQueryComparisonOp Comparison,
+		const TArray<FString>& AttributeValue) const
+	{
+		if(Comparison != EAccelByteV2SessionQueryComparisonOp::CONTAINS &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::NOT_CONTAINS)
+		{
+			UE_LOG(LogAccelByte, Warning, TEXT("Invalid session query operator/type combination!"));
+			return;
+		}
+
+		TArray<TSharedPtr<FJsonValue>> JsonArray;
+		for(const FString& Item : AttributeValue)
+		{
+			JsonArray.Add(MakeShared<FJsonValueString>(Item));
+		}
+
+		SET_SESSION_QUERY_FIELD(Array, JsonArray);
+	}
+
+	void AddParam(
+		const FString& AttributeName,
+		const EAccelByteV2SessionQueryComparisonOp Comparison,
+		const TArray<double>& AttributeValue) const
+	{
+		if(Comparison != EAccelByteV2SessionQueryComparisonOp::CONTAINS &&
+			Comparison != EAccelByteV2SessionQueryComparisonOp::NOT_CONTAINS)
+		{
+			UE_LOG(LogAccelByte, Warning, TEXT("Invalid session query operator/type combination!"));
+			return;
+		}
+
+		TArray<TSharedPtr<FJsonValue>> JsonArray;
+		for(const double Item : AttributeValue)
+		{
+			JsonArray.Add(MakeShared<FJsonValueNumber>(Item));
+		}
+
+		SET_SESSION_QUERY_FIELD(Array, JsonArray);
+	}
+
+#undef SET_SESSION_QUERY_FIELD
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AccelByte | Session | Models | SessionQuery")
 		FJsonObjectWrapper JsonWrapper;
+
+private:
+	TSharedPtr<FJsonObject> InitializeComparisonField(const EAccelByteV2SessionQueryComparisonOp Op, FString& OutFieldName) const
+	{
+		if(!JsonWrapper.JsonObject->HasTypedField<EJson::Object>(TEXT("query")))
+		{
+			JsonWrapper.JsonObject->SetObjectField(TEXT("query"), MakeShared<FJsonObject>());
+		}
+
+		const TSharedPtr<FJsonObject> QueryObject = JsonWrapper.JsonObject->GetObjectField(TEXT("query"));
+
+		switch(Op)
+		{
+		case EAccelByteV2SessionQueryComparisonOp::EQUAL:
+			OutFieldName = TEXT("equals");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::NOT_EQUAL:
+			OutFieldName = TEXT("notEquals");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::GREATER_THAN:
+			OutFieldName = TEXT("greaterThan");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::GREATER_THAN_EQUAL:
+			OutFieldName = TEXT("greaterThanEquals");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::LESS_THAN:
+			OutFieldName = TEXT("lessThan");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::LESS_THAN_EQUAL:
+			OutFieldName = TEXT("lessThanEquals");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::CONTAINS:
+			OutFieldName = TEXT("in");
+			break;
+		case EAccelByteV2SessionQueryComparisonOp::NOT_CONTAINS:
+			OutFieldName = TEXT("notIn");
+			break;
+		default: break;
+		}
+
+		if(!QueryObject->HasTypedField<EJson::Object>(OutFieldName))
+		{
+			QueryObject->SetObjectField(OutFieldName, MakeShared<FJsonObject>());
+		}
+
+		return QueryObject->GetObjectField(OutFieldName);
+	}
+
+	void AddToQuery(const FString& ComparisonFieldName, const TSharedPtr<FJsonObject>& ComparisonField) const
+	{
+		// #NOTE: The AddToQuery method should only ever be called after InitializeComparisonField has been called, but
+		// putting this here just in case something changes in the future
+		if(!JsonWrapper.JsonObject->HasTypedField<EJson::Object>(TEXT("query")))
+		{
+			JsonWrapper.JsonObject->SetObjectField(TEXT("query"), MakeShared<FJsonObject>());
+		}
+
+		const TSharedPtr<FJsonObject> QueryObject =	JsonWrapper.JsonObject->GetObjectField(TEXT("query"));
+		QueryObject->SetObjectField(ComparisonFieldName, ComparisonField);
+
+		JsonWrapper.JsonObject->SetObjectField(TEXT("query"), QueryObject);
+	}
 };
 
 USTRUCT(BlueprintType)
