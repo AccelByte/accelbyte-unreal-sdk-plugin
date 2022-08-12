d4f967ff23405a21ef9cbacebec4a93ddb2ae9c5
Jordan Fitzgerald <jordan@accelbyte.net>
2022-08-12 16:57:46 +0000

    Merged in feature/add-game-session-invite (pull request #387)
    add game session invite endpoint to session api
    
    * add game session invite endpoint to session api
    
    
    Approved-by: Wijanarko Sukma Pamungkas
    Approved-by: Maxwell Flynn

--- Source/AccelByteUe4Sdk/Private/Api/AccelByteSessionApi.cpp
+++ Source/AccelByteUe4Sdk/Private/Api/AccelByteSessionApi.cpp
@@ -143,6 +143,21 @@ void Session::DeleteGameSession(
 	HttpClient.ApiRequest(TEXT("DELETE"), Url, {}, FString(), OnSuccess, OnError);
 }
 
+void Session::SendGameSessionInvite(
+	FString const& GameSessionID,
+	FString const& UserID,
+	FVoidHandler const& OnSuccess,
+	FErrorHandler const& OnError)
+{
+	FReport::Log(FString(__FUNCTION__));
+
+	const FAccelByteModelsV2SessionInviteRequest RequestBody = {UserID};
+	const FString Url = FString::Printf(TEXT("%s/v1/public/namespaces/%s/gamesessions/%s/invite"),
+		*SettingsRef.SessionServerUrl, *CredentialsRef.GetNamespace(), *GameSessionID);
+
+    HttpClient.ApiRequest(TEXT("POST"), Url, {}, RequestBody, OnSuccess, OnError);
+}
+
 void Session::JoinGameSession(
 	FString const& GameSessionID,
 	THandler<FAccelByteModelsV2GameSession> const& OnSuccess,
@@ -243,7 +258,7 @@ void Session::SendPartyInvite(
 {
 	FReport::Log(FString(__FUNCTION__));
 
-	const FAccelByteModelsV2PartyInviteRequest RequestBody = {InviteeID};
+	const FAccelByteModelsV2SessionInviteRequest RequestBody = {InviteeID};
 	const FString Url = FString::Printf(TEXT("%s/v1/public/namespaces/%s/parties/%s/invite"),
 		*SettingsRef.SessionServerUrl, *CredentialsRef.GetNamespace(), *PartyID);
 
